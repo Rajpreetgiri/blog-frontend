d7299fbda54cecf83923a1f49956e2166703f025
Rajpreetgiri <rajpreetgiri4@gmail.com>
2024-02-15 01:00:08 +0530

    code spliting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,28 @@
-// Third-party
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-// Layout
 import AppLayout from '../layout';
 
-// Pages
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Errorpage from "../pages/Errorpage"
-import BlogCreate from '../pages/Blog/create';
-import BlogEdit from '../pages/Blog/edit';
+// Lazy-loaded components
+const Home = lazy(() => import('../pages/Home'));
+const Login = lazy(() => import('../pages/Login'));
+const Errorpage = lazy(() => import('../pages/Errorpage'));
+const BlogCreate = lazy(() => import('../pages/Blog/create'));
+const BlogEdit = lazy(() => import('../pages/Blog/edit'));
 
 function AllRoutes() {
     return (
         <AppLayout>
             <Routes>
                 {/* Guest Routes */}
-                <Route path="/" element={<Home />} />
-                <Route path='*' element={<Errorpage />} />
+                <Route path="/" element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
+                <Route path='*' element={<Suspense fallback={<div>Loading...</div>}><Errorpage /></Suspense>} />
 
                 {/* Protected Routes */}
-                <Route path={`/blog-create/`} element={<BlogCreate />} />
-                <Route path={`/blog-edit/:slug`} element={<BlogEdit />} />
+                <Route path="/blog-create/" element={<Suspense fallback={<div>Loading...</div>}><BlogCreate /></Suspense>} />
+                <Route path="/blog-edit/:slug" element={<Suspense fallback={<div>Loading...</div>}><BlogEdit /></Suspense>} />
 
                 {/* Auth Routes */}
-                <Route path="/login" element={<Login />} />
-
+                <Route path="/login" element={<Suspense fallback={<div>Loading...</div>}><Login /></Suspense>} />
             </Routes>
         </AppLayout>
     );
